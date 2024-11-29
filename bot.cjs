258acb203b53597e49c390251443056c69bfdd7f
yunnosnd <fbradolfo@gmail.com>
2024-11-29 19:01:34 +0000

    Implementação LocalAuth e testes de disponibilização de QRCode

--- bot.cjs
+++ bot.cjs
@@ -1,11 +1,57 @@
-const qrcode = require('qrcode-terminal');
-const { Client } = require('whatsapp-web.js');
+const qrcodeTerminal = require('qrcode-terminal');
+const qrcode = require('qrcode');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 
+const client = new Client({authStrategy: new LocalAuth()});
 
-const client = new Client();
+client.on('qr', async (qr) => {
+    qrcodeTerminal.generate(qr, { small: true });
+    qrcode.toBuffer(qr, {
+        width: 10,
+        height: 10,
+        margin: 1,
+        color: {
+            dark: '#000',  // Cor do primeiro plano
+            light: '#fff'  // Cor do fundo
+        }
+    }, (err, imgBuffer) => {
+        if (err) throw err;
 
-client.on('qr', (qr) => {
-    qrcode.generate(qr, { small: true });
+        // Salva a imagem em um arquivo
+    require('fs').writeFileSync('.wwebjs_auth/qr_code.png', imgBuffer);
+    console.log('QR code gerado com sucesso!');
+    });
+
+    // Disponibiliza a imagem do QR Code em um link para ser acessado pelo navegador
+    const http = require('http');
+    const fs = require('fs');
+    const server = http.createServer((req, res) => {
+        res.writeHead(200, {'Content-Type': 'image/png'});
+        fs.createReadStream('.wwebjs_auth/qr_code.png').pipe(res);
+    });
+    server.listen(3000, () => {
+        console.log('Servidor rodando na porta 3000');
+    });
+
+    // Mostra no console o link para acessar o QR Code
+    console.log('Acesse o link abaixo para escanear o QR Code:');
+    console.log('http://localhost:3000');
+
+    const notifier = require('node-notifier');
+    const path = require('path');
+
+    // Função para exibir notificação
+    function showNotification(title, message) {
+        notifier.notify({
+            title: title,
+            message: message,
+            sound: true, // Som opcional
+            wait: true // Espera até que o usuário clique na notificação
+        });
+    }
+
+    // Exemplo de uso
+    showNotification('QR Code Gerado', 'O QR code foi gerado com sucesso e está disponível.');
 });
 
 client.on('ready', () => {
@@ -13,23 +59,36 @@ client.on('ready', () => {
 });
 
 client.on('message', async (msg) => {
-    const chat = (await msg.getContact()).number;
-    const user = chat.number;
     const chatId = msg.from;
 
-    console.log(typeof user);
-    console.log(user);
 
-    console.log(chatId);
 
-    if (chatId == "" && msg.body == 'ping') {
-        await msg.reply('pong');
-    };
-    if (chatId == "" && msg.body == 'teste') {
-        await client.sendMessage(chatId, 'Testado');
-    };
+    // if (chatId == ""+"@c.us") {
+    //     await client.sendMessage(chatId, 'Olá! Eu sou o bot de atendimento da W-Sinos, em que posso te ajudar?');
+    //     await client.sendMessage(chatId, '1 - Teste' + '\n' + '2 - Testado' + '\n' + '3 - Atendimento');
+    // };
 
-})
+    // Se não for a primeira mensagem do chatid, e for 1, 2 ou 3, ele vai responder
+
+
+    // if (chatId == ""+"@c.us" && msg.body == '1') {
+    //     await client.sendMessage(chatId, 'Teste');
+    // }
+    // if (chatId == ""+"@c.us" && msg.body == '2') {
+    //     await client.sendMessage(chatId, 'Testado');
+    // }
+    // if (chatId == ""+"@c.us" && msg.body == '3') {
+    //     await client.sendMessage(chatId, 'Aguarde um momento, por favor');
+    //     console.log('Aqui foi chamado o atendente');
+    // }
+
+    // if (chatId == "" && msg.body == 'ping') {
+    //     await msg.reply('pong');
+    // };
+    // if (chatId == "" && msg.body == 'teste') {
+    //     await client.sendMessage(chatId, 'Testado');
+    // };
+});
 
 // Start your client
 client.initialize();
